6536b8b4ac1aaf0ef98564597cb8e58dc2962a42
XunRong Tan <tanxrong@gmail.com>
2014-09-03 16:08:12 +0800

    add load_string & close

--- Makefile
+++ Makefile
@@ -2,13 +2,13 @@
 all : leela
 
 leela : libmruby
-	rustc leela.rs -L ./mruby/build/host/lib
+	rustc leela.rs -L ./mruby/build/host/lib -o leela.out
 
 libmruby : 
 	if [ ! -d "./mruby" ];then git clone --depth=1 https://github.com/mruby/mruby;fi
 	cd ./mruby && export CFLAGS=$(CFLAGS)" -fPIC" && make
 
 clean : 
-	rm leela
+	rm leela.out
 
 .PHONY : all leela clean
--- leela.rs
+++ leela.rs
@@ -1,18 +1,29 @@
 
+extern crate libc;
 mod mruby
 {
 
+use libc::{c_char};
+
 #[repr(C)]
     struct mrb_state;
+#[repr(C)]
+    struct mrb_value;
 
 #[link(name="m")]
 #[link(name="mruby")]
     extern "C" {
         fn mrb_open() -> *mut mrb_state;
+        fn mrb_load_string(state:*mut mrb_state,s:*const c_char) -> mrb_value;
+        fn mrb_close(state:*mut mrb_state);
     }
 
     pub struct Mrb{
         state:*mut mrb_state
+    }
+
+    pub struct Value {
+        val :*mut mrb_state
     }
 
     pub fn open() -> Mrb {
@@ -21,9 +32,24 @@ mod mruby
     }
 
     impl Mrb {
+        pub fn load_str(&self,s:&str) {
+            let cs = s.to_c_str();
+            unsafe {
+                mrb_load_string(self.state,cs.as_ptr());
+            }
+        }
+
+        pub fn close(&self) {
+            unsafe {
+                mrb_close(self.state);
+            }
+        }
     }
 }
 
 fn main() {
-    let m = mruby::open();
+    let mut m = mruby::open();
+    let exec = "[1,2,3].each do |i| puts i+1 end";
+    m.load_str(exec);
+    m.close();
 }
