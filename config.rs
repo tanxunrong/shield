33a6ab92434ec295289e8abf8d3fb66d220dba5b
tanxunrong <tanxrong@gmail.com>
2014-09-05 21:18:19 +0800

    add mut to &self,works

--- config.rs
+++ config.rs
@@ -1,43 +1,56 @@
 
-#[crate_type="lib"]
 use std::collections::{HashMap};
 
+
+#[deriving(PartialEq,Eq,Clone,Show)]
 enum PartType {
-    DIV, // divide to int
-    MOD // remainder
+    DIV = 1, // divide to int
+    MOD = 2// remainder
 }
 
+#[deriving(PartialEq,Eq,Clone,Show)]
 struct TableCfg {
     ptype:PartType,
     num:uint,
     col:String
 }
 
+#[deriving(PartialEq,Eq,Clone,Show)]
 struct Table {
     pri : Vec<String>, // primary keys
     name : String,
     cfg : TableCfg
 }
 
-struct Conf {
-    tbls : HashMap<String,Table>,
+#[deriving(PartialEq,Eq,Clone,Show)]
+pub struct Conf {
+    tbls :  HashMap<String,Table>,
     db:String
 }
 
 pub fn new(dbname:& str) -> Conf {
-    Conf{tbls:HashMap::new(),db:String::from_str(dbname)}
+    let t = HashMap::new(); 
+    Conf{tbls:t,db:String::from_str(dbname)}
 }
 
 impl Conf {
-    fn add_tbl(&self,tb:&str,col:&str,t:PartType,num:uint) {
+    fn add_tbl(&mut self,tb:&str,col:&str,t:PartType,num:uint) {
         let cfg = TableCfg{ptype:t,num:num,col:String::from_str(col)};
-        let t = Table { pri:Vec::new(),name:String::from_str(tb),cfg:cfg};
+        let mut t = Table { pri:Vec::new(),name:String::from_str(tb),cfg:cfg};
+        t.pri.push(String::from_str(tb));
         self.tbls.insert(String::from_str(tb),t);
     }
 }
 
-#[cfg(test)]
-fn test() {
-    let c = new("pirate");
+#[test]
+fn test_conf_init() {
+    let mut c = new("pirate");
     c.add_tbl("bag","uid",MOD,10);
 }
+
+fn main() {
+    let mut c = new("pirate");
+    c.add_tbl("bag","uid",MOD,10);
+    println!("conf {}",c);
+}
+
