4f1fe3ed1ecf095a0cb939e46a9d297117af9cd3
tanxunrong <tanxrong@gmail.com>
2014-09-06 10:06:03 +0800

    parse table

--- src/config.rs
+++ src/config.rs
@@ -27,22 +27,26 @@ pub struct Conf {
     db:String
 }
 
-pub fn new(dbname:& str) -> Conf {
-    let t = HashMap::new(); 
-    Conf{tbls:t,db:String::from_str(dbname)}
-}
 
 impl Conf {
-    fn add_tbl(&mut self,tb:&str,col:&str,t:PartType,num:uint) {
+
+    //static func
+    pub fn new(dbname:& str) -> Conf {
+        let t = HashMap::new(); 
+        Conf{tbls:t,db:String::from_str(dbname)}
+    }
+
+    pub fn add_tbl(&mut self,tb:&str,col:&str,t:PartType,num:uint) {
         let cfg = TableCfg{ptype:t,num:num,col:String::from_str(col)};
         let mut t = Table { pri:Vec::new(),name:String::from_str(tb),cfg:cfg};
         t.pri.push(String::from_str(tb));
         self.tbls.insert(String::from_str(tb),t);
+        //println!("t {}",t); t moved
     }
 }
 
 #[test]
 fn test_conf_init() {
-    let mut c = new("pirate");
+    let mut c = Conf::new("pirate");
     c.add_tbl("bag","uid",MOD,10);
 }
--- src/parse.rs
+++ src/parse.rs
@@ -1,15 +1,53 @@
 
+//use std::fmt::{Formatter,FormatError};
+//use std::str::CharRange;
+
+#[deriving(PartialEq,Eq,Clone,Show)]
 pub struct Query {
     orig:String,
     table:String
 }
 
-pub fn new_sql(q:&str) -> Query {
-    Query{orig:String::from_str(q),table:String::new()}
+
+#[deriving(PartialEq,Eq,Clone,Show)]
+pub enum ParseError {
+    DelMethod,
+    ErrFormat
+}
+
+pub type ParseResult = Result<Query,ParseError>;
+
+impl Query {
+    pub fn parse(sql:&str) -> ParseResult {
+        let s = sql.trim_chars([' ','\t','\n','\r'].as_slice());
+        if s.len() < 6
+        {
+            return Err(ErrFormat);
+        }
+        let method = s.slice_chars(0,6);
+        if method == "delete" {
+            return Err(DelMethod);
+        }
+        else if method == "update" {
+            let v:Vec<&str> = s.split(' ').collect();
+            let q = Query{orig:String::from_str(s),table:String::from_str(v[1])};
+            return Ok(q);
+        }
+        else {
+            return Err(ErrFormat);
+        }
+   }
 }
 
 #[test]
-fn test_init_sql() {
-    let q = "select * from t_bag where uid = 20111;";
-    let mut s =  new_sql(q);
+fn test_sql_parse() {
+    let q = "update t_bag set item_id = 0 where uid = 20100;";
+    let ret = Query::parse(q);
+    println!("parse ret : {}",ret);
+
+    match ret {
+        Ok(v) => { assert!(v.table.as_slice() == "t_bag"); } 
+        Err(e) => { fail!("expected ok,parse err {}",e); } 
+    }
 }
+
