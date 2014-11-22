67cae5b312a0b843561874b233aa3d313aa78208
tanxunrong <tanxrong@gmail.com>
2014-11-22 09:09:07 +0800

    add cfg(target_arch),pass open test

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,12 @@
 //! mruby binding
 
 extern crate libc;
+mod mruby;
 
-pub mod mruby;
+#[test]
+fn test_mrb_open() {
+    unsafe {
+        let mrb : *mut mruby::mrb_state = mruby::mrb_open();
+        mruby::mrb_close(mrb);
+    }
+}
