454ba33b7e7f3f619cd3a55074fa333c0b99eda5
tanxr <tanxrong@gmail.com>
2014-11-24 11:36:55 +0800

    add Mrb open&close

--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,28 @@
 extern crate libc;
 mod mruby;
 
+pub struct Mrb {
+    mrb : *mut mruby::mrb_state,
+}
+
+pub fn new_mrb() -> Mrb {
+    let mrb = unsafe {
+        mruby::mrb_open()
+    };
+    Mrb{mrb:mrb}
+}
+
+impl Mrb {
+    fn close(&self) {
+        unsafe {
+            mruby::mrb_close(self.mrb);
+        }
+        std::mem::drop(self);
+    }
+}
+
 #[test]
 fn test_mrb_open() {
-    unsafe {
-        let mrb : *mut mruby::mrb_state = mruby::mrb_open();
-        mruby::mrb_close(mrb);
-    }
+    let m = new_mrb();
+    m.close();
 }
