9e4c2df390db579fce07edcf47fa3f13d0a6a182
tanxr <tanxrong@gmail.com>
2014-11-24 12:28:40 +0800

    add ToValue trait

--- src/lib.rs
+++ src/lib.rs
@@ -3,15 +3,16 @@
 //! mruby binding
 
 extern crate libc;
+use mruby::{mrb_open,mrb_close,mrb_state,mrb_value};
 mod mruby;
 
 pub struct Mrb {
-    mrb : *mut mruby::mrb_state,
+    mrb : *mut mrb_state,
 }
 
 pub fn new_mrb() -> Mrb {
     let mrb = unsafe {
-        mruby::mrb_open()
+        mrb_open()
     };
     Mrb{mrb:mrb}
 }
@@ -19,9 +20,32 @@ pub fn new_mrb() -> Mrb {
 impl Mrb {
     fn close(&self) {
         unsafe {
-            mruby::mrb_close(self.mrb);
+            mrb_close(self.mrb);
         }
         std::mem::drop(self);
+    }
+
+}
+
+pub trait ToValue {
+    fn to_mvalue(&self,m:Mrb) -> mrb_value;
+}
+
+impl ToValue for String {
+    fn to_mvalue(&self,m:Mrb) -> mrb_value {
+        unsafe {
+            mruby::mrb_str_new(m.mrb,
+                                self.as_slice().as_ptr() as *const libc::c_char,
+                                self.len() as libc::size_t)
+        }
+    }
+}
+
+impl <V> ToValue for V where V:std::num::Int {
+    fn to_mvalue(&self,m:Mrb) -> mrb_value {
+        unsafe {
+
+        }
     }
 }
 
