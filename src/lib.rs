eaa78e0f1bcebdbbbe4884c74b82d8d280f57b73
tanxunrong <tanxrong@gmail.com>
2014-11-29 08:42:21 +0800

    add map<name,class>

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,11 @@
 
-//! # mruby
-//! mruby binding
+#![feature(unsafe_destructor)]
 #[allow(dead_code)]
+
 extern crate libc;
 use std::rc::{Rc};
 use std::cell::{RefCell};
+use std::collections::{HashMap};
 use mruby::{mrb_open,mrb_close,mrb_state,mrb_value,Struct_RClass};
 mod mruby;
 
@@ -12,90 +13,93 @@ pub type MrbState = Rc<RefCell<*mut mrb_state>>;
 pub type MrbClass = Rc<RefCell<*mut Struct_RClass>>;
 
 pub struct Mrb {
-    mrb : MrbState
-}
-
-pub fn new_mrb() -> Mrb {
-    let mrb = unsafe {
-        mrb_open()
-    };
-    Mrb{ mrb:Rc::new(RefCell::new(mrb)) }
+    mrb : MrbState,
+    clzs : RefCell<HashMap<String,MrbClass>>
 }
 
 pub struct Class {
     mrb : MrbState,
-    clz : MrbClass,
-    outer : MrbClass
+    clz : MrbClass
 }
 
 impl Mrb {
 
-    #[inline(always)]
-    fn get_mrb(&self) -> *mut mrb_state {
-        unsafe { *(self.mrb).borrow_mut() } 
+    pub fn new() -> Mrb {
+        let mrb = unsafe {
+            mrb_open()
+        };
+        let state = unsafe { *mrb };
+        let clzs : RefCell<HashMap<String,MrbClass>> = RefCell::new(HashMap::new());
+        let m = Mrb{ mrb:Rc::new(RefCell::new(mrb)),clzs:clzs };
+        m.clzs.borrow_mut().insert(String::from_str("Object"),Rc::new(RefCell::new(state.object_class)));
+        m.clzs.borrow_mut().insert(String::from_str("Class"),Rc::new(RefCell::new(state.class_class)));
+        m.clzs.borrow_mut().insert(String::from_str("String"),Rc::new(RefCell::new(state.string_class)));
+        m.clzs.borrow_mut().insert(String::from_str("Array"),Rc::new(RefCell::new(state.array_class)));
+        m.clzs.borrow_mut().insert(String::from_str("Hash"),Rc::new(RefCell::new(state.hash_class)));
+        m
     }
 
-    pub fn close(&self) {
-        unsafe {
-            mrb_close(self.get_mrb());
-        }
-        std::mem::drop(self);
+    #[inline(always)]
+    fn get_mrb(&self) -> *mut mrb_state {
+        *(self.mrb).borrow_mut()
     }
 
     pub fn get_class(&self,name:&str) -> Option<Class> {
-        let mrb = unsafe { *self.get_mrb() };
-        let class = 
-            match name {
-                "object" => { mrb.object_class },
-                "class" => { mrb.class_class },
-                "string" => { mrb.string_class },
-                "array" => { mrb.array_class },
-                "hash" => { mrb.hash_class },
-                _ => {
-                    unsafe { mruby::mrb_class_get(self.get_mrb(),name.as_slice().as_ptr() as *const libc::c_char) }
-                }
-            };
-        if class.is_null() {
+        let mut h = self.clzs.borrow_mut();
+        let clzname = String::from_str(name);
+        if h.get(&clzname).is_some() {
+                return Some(Class{mrb:self.mrb.clone(),clz:h.get(&clzname).unwrap().clone()})
+        }
+        let mclz = unsafe { 
+            mruby::mrb_class_get(self.get_mrb(), 
+                                 name.as_slice().as_ptr() as *const libc::c_char) 
+        };
+        if mclz.is_null() {
             None
         } else {
-            Some(Class{mrb:self.clone(),clz:Rc::new(RefCell::new(class)))
+            let class = Class{mrb:self.mrb.clone(),clz:Rc::new(RefCell::new(mclz))};
+            h.insert(clzname,class.clz.clone());
+            Some(class)
         }
     }
 
     pub fn load(&self,code:&str) -> mrb_value {
         unsafe {
-            mruby::mrb_load_string(self.get_mrb(),code.as_slice().as_ptr() as *const libc::c_char)
+            mruby::mrb_load_string(self.get_mrb(),
+            code.as_slice().as_ptr() as *const libc::c_char)
         }
     }
 
-    pub fn def_class(&self,clzname:&str,outer:Option<Class>) -> Class {
-        let super_clz = match outer {
-            Some(c) => c,
-            None() => { 
 }
 
+#[unsafe_destructor]
+impl Drop for RefCell<*mut mrb_state> {
+    fn drop(&mut self) {
+        let mrb = *self.borrow_mut();
+        unsafe {
+            mrb_close(mrb);
+        }
+    }
+}
 
 #[test]
 fn test_mrb_open() {
-    let m = new_mrb();
-    m.close();
+    let m = Mrb::new();
 }
 
 #[test]
 fn test_get_class() {
-    let m = new_mrb();
-    assert!(m.get_class("class").is_some())
-    assert!(m.get_class("object").is_some())
-    assert!(m.get_class("hash").is_some())
-    assert!(m.get_class("array").is_some())
-    assert!(m.get_class("string").is_some())
-    m.close();
+    let m = Mrb::new();
+    assert!(m.get_class("Class").is_some());
+    assert!(m.get_class("Object").is_some());
+    assert!(m.get_class("Hash").is_some());
+    assert!(m.get_class("Array").is_some());
+    assert!(m.get_class("String").is_some());
 }
 
 #[test]
 fn test_load_str() {
-    let m = new_mrb();
+    let m = Mrb::new();
     let mut v = m.load("1..3.each do |i| puts i end");
     assert!(v.is_nil());
-    m.close();
 }
