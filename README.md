5eb4d4d1c7ad84cf547883f309aeb291fee42e56
tanxunrong <tanxrong@gmail.com>
2014-12-18 15:53:26 +0800

    fix libmrb build.rs

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ shield
 ======
 
 mruby binding in rust lang.It's in very alpha state.
+
+build
+-----
+first,compile mruby with -fPIC flags and make sure *libmruby.a* is somewhere in *LD_LIBRARY_PATH*.
+then,set *MRUBY_ROOT* env variable to the dir of mruby source code.
+And *cargo test*.
+
--- build.rs
+++ build.rs
@@ -9,10 +9,10 @@ fn main() {
     cflags.push_str(" -fPIC -std=c99");
     setenv("CFLAGS", cflags);
 
-    let mruby_root = getenv("MRUBY_ROOT").unwrap_or("/usr".to_string());
-    let hdir = mruby_root.push_str("/include");
+    let mut mruby_root = getenv("MRUBY_ROOT").unwrap_or("/usr".to_string());
+    mruby_root.push_str("/include");
 
     gcc::compile_library("libmrb.a", &gcc::Config {
-        include_directories: vec!(Path::new(hdir)), .. Default::default()
+        include_directories: vec!(Path::new(mruby_root)), .. Default::default()
     }, &["mrb/lib.c"]);
 }
